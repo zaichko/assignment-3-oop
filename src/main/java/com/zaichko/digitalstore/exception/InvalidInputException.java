91abbbb79e0c537de735187e3e215a73216db906
Yekaterina Zaichko <245034547+zaichko@users.noreply.github.com>
2026-02-10 16:21:40 +0500

    Reset repository: remove old project

--- src/main/java/com/zaichko/digitalstore/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package com.zaichko.digitalstore.exception;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/zaichko/digitalstore/repository/interfaces/UserRepositoryInterface.java
+++
@@ -1,7 +0,0 @@
-package com.zaichko.digitalstore.repository.interfaces;
-
-import com.zaichko.digitalstore.model.User;
-
-public interface UserRepositoryInterface extends CrudRepository<User> {
-    User getByEmail(String email);
-}
